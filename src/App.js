d89167179fc255529be9d0d78b50b4a093721c53
Deepanshu <deepanshu.kewalramani2@gmail.com>
2024-06-15 17:30:36 +0530

    added the about section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import CSVInput from './components/CSVInput';
-import MapComponent from './components/MapComponent';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import CSVInput from "./components/CSVInput";
+import MapComponent from "./components/MapComponent";
+import About from "./components/About";
 
 const App = () => {
   const [datasets, setDatasets] = useState([]);
 
   const addDataset = (newDataset) => {
-    setDatasets(prevDatasets => [...prevDatasets, newDataset]);
+    setDatasets((prevDatasets) => [...prevDatasets, newDataset]);
   };
 
   return (
-    <div className="min-h-screen bg-gray-200">
-      <Navbar />
-      <div className="flex flex-col items-center pt-8">
-        {datasets.length === 0 ? (
-          <CSVInput addDataset={addDataset} />
-        ) : (
-          <MapComponent datasets={datasets} />
-        )}
+    <Router>
+      <div className="min-h-screen bg-gray-200">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="flex flex-col items-center pt-8">
+                {datasets.length === 0 ? (
+                  <CSVInput addDataset={addDataset} />
+                ) : (
+                  <MapComponent datasets={datasets} />
+                )}
+              </div>
+            }
+          />
+
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 };
 
